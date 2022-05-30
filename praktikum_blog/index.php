f667da06c96f0262f6277e66405904e2b0ff99fb
DDjuni <www.dedejuni2002@gmail.com>
2022-05-30 10:51:49 +0800

    mentahan

---
+++ praktikum_blog/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="header">
+ <h1>My Blog</h1>
+ <p>Let's Share Story</p>
+</div>
+ 
+<!-- Menampikan kolom sebelah kiri -->
+<div class="row">
+ <div class="leftcolumn">
+   <div class="card">
+     <a href="post.php"><h2>HARI PERTAMA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>HARI KEDUA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>HARI KETIGA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>HARI KEEMPAT</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>HARI KELIMA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+ </div>
+ 
+ <!-- Menampikan kolom sebelah kanan -->
+ <div class="rightcolumn">
+   <div class="card">
+     <h2>About Me</h2>
+     <div class="fakeimg" style="height:100px;">Image</div>
+     <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+   </div>
+   <div class="card">
+     <h3>Popular Post</h3>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+   </div>
+   <div class="card">
+     <h3>Follow Me on instagram</h3>
+     <p>@ddjuni_</p>
+   </div>
+ </div>
+</div>
+
+<div class="footer">
+ <h2></h2>
+</div>
+ 
+</body>
+</html>
